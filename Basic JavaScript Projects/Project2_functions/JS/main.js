c5c0b3aa3315cc88d6a5d15b91d2f5539c656800
SuperNate903 <minecraft.nate04@outlook.com>
2022-10-26 11:24:31 -0700

    Added a new button to concatenate a string

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,3 +1,9 @@
 function buttonPressed() {
     document.getElementById("text-line").innerHTML = "You pressed the button!"
 }
+
+function concatenateString() {
+    var sentence = "This is a sentence"
+    sentence += " with a concatenated string!"
+    document.getElementById("concatenated").innerHTML = sentence
+}
