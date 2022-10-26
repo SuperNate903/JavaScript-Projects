ad7c79df009163b3beb1e3d3695ff14d9cdd328f
SuperNate903 <minecraft.nate04@outlook.com>
2022-10-26 13:24:53 -0700

    Added comments

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -4,6 +4,6 @@ function dictionary() {
         String: "String",
         Float: 3.54
     }
-    delete Elements.String
-    document.getElementById("Dictionary").innerHTML = Elements.String
+    delete Elements.String // Deletes the string value
+    document.getElementById("Dictionary").innerHTML = Elements.String // Attempts to display string value
 }
