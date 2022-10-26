39392be17ae30264098f238563b4b9d16927c3eb
SuperNate903 <minecraft.nate04@outlook.com>
2022-10-25 18:24:12 -0700

    Added my first project file

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+window.alert("Hello, World!");
+document.write("Hello, World!");
+
+var A = "<br>This is a string!";
+document.write(A);
+
+var Alert = "Alert box";
+window.alert(Alert);
+
+document.write("<br>I am just a string, " + "but I\'ve been concatenated!")
+
+var B = "Concatenated" + " String";
+document.write(B);
+
+var num1 = 5, num2 = 13, num3 = 39
+document.write("<br>"+num2); // This is printing the second number in the var list above
+
+document.write("<br>")
+document.write(3+12);
