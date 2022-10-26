bf330c497aeda5c343ca11a97ded8ce2c9371e54
SuperNate903 <minecraft.nate04@outlook.com>
2022-10-26 11:26:38 -0700

    Added comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,9 +1,9 @@
 function buttonPressed() {
-    document.getElementById("text-line").innerHTML = "You pressed the button!"
+    document.getElementById("text-line").innerHTML = "You pressed the button!" // Displays text on paragraph element
 }
 
 function concatenateString() {
-    var sentence = "This is a sentence"
-    sentence += " with a concatenated string!"
-    document.getElementById("concatenated").innerHTML = sentence
+    var sentence = "This is a sentence" // Sets the sentence variable
+    sentence += " with a concatenated string!" // Adds concatenated string to sentence
+    document.getElementById("concatenated").innerHTML = sentence // Displays sentence on paragraph element
 }
